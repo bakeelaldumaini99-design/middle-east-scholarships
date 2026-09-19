b82bdead74093762451dd55b2bafebab90974bef
bakeelaldumaini99-design <bakeelaldumaini99@gmail.com>
2026-09-19 17:57:15 +0300

    بيانات محتوى الموقع وصفحة الهوم

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import Hero from '@/sections/Hero'
+import Universities from '@/sections/Universities'
+import Bachelor from '@/sections/Bachelor'
+import ChineseProgram from '@/sections/ChineseProgram'
+import { Steps, Footer } from '@/sections/Steps'
+import '../App.css'
+
+export default function Home() {
+  return (
+    <div dir="rtl">
+      <Hero />
+      <Universities />
+      <Bachelor />
+      <ChineseProgram />
+      <Steps />
+      <Footer />
+    </div>
+  )
+}
